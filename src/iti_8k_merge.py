27f1a0c1806cc0deb22e010d76c2de2f8e24359f
Wfiles <william.jallot@epfl.ch>
2025-12-29 15:40:47 +0100

    added data for iti 8k merged

--- src/iti_8k_merge.py
+++ src/iti_8k_merge.py
@@ -2,6 +2,7 @@ from src.sec_8k_preprocess import preprocess_sec_8k, preprocess_sec_8k_nlp
 from src.iti_preprocess import prepare_ITI_data
 import polars as pl
 
+
 def merge_8k_iti() -> pl.DataFrame:
     """Merge SEC 8-K filings with ITI data on PERMNO and report date."""
     
@@ -16,9 +17,15 @@ def merge_8k_iti() -> pl.DataFrame:
     first_8k_date = df_8k.select(pl.col("report_date").min() - pl.duration(weeks=2)).item()
     df_iti = df_iti.filter(pl.col("date") >= first_8k_date)
     
-
+    from pathlib import Path
     # Merge on 'permno' and 'report_date'
     df_merged = df_iti.join(df_8k, left_on=['permno', 'date'], right_on=['permno', 'report_date'], how='left')
+    output_dir = Path("data/merged")
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    output_path = output_dir / "8k_iti.parquet"
+    df_merged.write_parquet(output_path)
+
     return df_merged
 
 
